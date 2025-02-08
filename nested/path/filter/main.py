76d3a692a44d61421b851a443d8c2b00f640d00c
Nusiq <nusiq@outlook.com>
2025-02-08 13:39:50 +0100

    hello-nested-path-filter fix.

--- nested/path/filter/main.py
+++ nested/path/filter/main.py
@@ -3,11 +3,11 @@ from pathlib import Path
 VERSION = "1.0.0"
 
 def main():
-    with Path("data/hello-release-filter/message.txt").open(
+    with Path("data/hello-nested-path-filter/message.txt").open(
         "r", encoding="utf8"
     ) as f:
         message = f.read()
-    with Path("BP/hello_release_filter.txt").open("w", encoding="utf8") as f:
+    with Path("BP/hello_nested_path_filter.txt").open("w", encoding="utf8") as f:
         f.write(
             f"Version: {VERSION}\n"
             f"Message: {message.strip()}\n"
